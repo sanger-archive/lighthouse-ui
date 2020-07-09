f4023e3032f203993f32351ad0bc345165afbd0e
Stephen Inglis <si5@sanger.ac.uk>
2020-07-09 11:16:39 +0100

    First commit. Moved from gitlab.

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+const { CI_PAGES_URL } = process.env
+const base = CI_PAGES_URL && new URL(CI_PAGES_URL).pathname
+
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Lighthouse reports',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Customize the generated output folder
+   */
+  generate: {
+    dir: 'public'
+  },
+
+  /*
+   ** Customize the base url
+   */
+  router: {
+    base
+  },
+
+  /*
+   ** Global CSS
+   */
+  css: [],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {
+    browserBaseURL: 'https://lighthouse.psd.sanger.ac.uk/'
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) { }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1>Lighthouse positive samples reports</h1>
+        <p class="lead">
+          Reports generated for Lighthouse samples which are on site
+        </p>
+        <p>
+          <b-button
+            variant="success"
+            :disabled="isCreating"
+            @click="createReport"
+          >
+            Create report
+            <b-spinner v-show="isCreating" small></b-spinner>
+          </b-button>
+          <b-button variant="info" :disabled="isCreating" @click="refreshTable">
+            Refresh
+          </b-button>
+        </p>
+        <p>
+          <b-alert :show="isError" dismissible variant="danger">
+            There was an error creating the report
+          </b-alert>
+          <b-alert :show="isCreated" dismissible variant="success">
+            Report successfully created
+          </b-alert>
+          <b-alert :show="isCreating" dismissible variant="warning">
+            Report creation takes about 30s to complete, please do not refresh
+            the page
+          </b-alert>
+        </p>
+        <b-table
+          ref="reports_table"
+          :items="reportsProvider"
+          :fields="fields"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          sort-icon-left
+          no-provider-sorting
+        >
+          <!-- A virtual column -->
+          <template v-slot:cell(index)="data">{{ data.index + 1 }}</template>
+
+          <template v-slot:cell(download_link)="data">
+            <b-button variant="primary" :href="data.item.download_url" download>
+              Download
+            </b-button>
+          </template>
+        </b-table>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sortBy: 'filename',
+      sortDesc: true,
+      fields: [
+        // A virtual column that doesn't exist in items
+        { key: 'index', label: '' },
+        { key: 'filename', sortable: true },
+        'size',
+        { key: 'download_link', label: '' }
+      ],
+      isCreating: false,
+      isError: false,
+      isCreated: false
+    }
+  },
+  methods: {
+    async reportsProvider(ctx) {
+      try {
+        const data = await this.$axios.$get(`reports`)
+        return data.reports
+      } catch (error) {
+        return []
+      }
+    },
+    async createReport() {
+      try {
+        this.isCreating = true
+        await this.$axios.$post(`reports/new`)
+        this.refreshTable()
+        this.isCreated = true
+        this.isCreating = false
+      } catch (error) {
+        this.isCreating = false
+        this.isError = true
+        return []
+      }
+    },
+    refreshTable() {
+      this.$refs.reports_table.refresh()
+    }
+  }
+}
+</script>
+
+<style></style>
