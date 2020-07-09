7a2dbfc15ec04e51dd90283558e6a7ecad457018
Stephen Inglis <si5@sanger.ac.uk>
2020-07-09 12:21:48 +0100

    Added comment.

--- test/pages/Index.spec.js
+++ test/pages/Index.spec.js
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils'
 import Index from '@/pages/Index.vue'
 
 describe('Index', () => {
+
+  // TODO: change to mount. Add stubs. Fix deprecation warnings.
   test('is a Vue instance', () => {
     const wrapper = shallowMount(Index)
     expect(wrapper.isVueInstance()).toBeTruthy()
