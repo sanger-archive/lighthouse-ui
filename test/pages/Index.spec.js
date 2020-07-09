c8c2121448e668d50a22b915988bb0aea9e53728
Stephen Inglis <si5@sanger.ac.uk>
2020-07-09 11:49:58 +0100

    Added basic test to check travis.

---
+++ test/pages/Index.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import Index from '@/pages/Index.vue'
+
+describe('Index', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(Index)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
