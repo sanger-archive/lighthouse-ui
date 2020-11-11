668867fa5cfa676a731f7125a7c657a823d568d8
Stephen Inglis <si5@sanger.ac.uk>
2020-11-11 17:27:56 +0000

    Added basic page for printing destination plate labels. Ready for tomorrow.

---
+++ pages/print_destination_plate_labels.vue
@@ -0,0 +1,73 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1>Print destination plate labels</h1>
+        <p class="lead">
+        </p>
+        <p>
+          <b-button
+            id="printLabels"
+            variant="success"
+            :disabled="isBusy"
+            @click="printLabels"
+          >
+            Create report
+            <b-spinner v-show="isBusy" small></b-spinner>
+          </b-button>
+        </p>
+        <!-- TODO: better in a component of its own? -->
+        <p>
+          <b-alert :show="isError" dismissible variant="danger">
+            {{ alertMessage }}
+          </b-alert>
+          <b-alert :show="isSuccess" dismissible variant="success">
+            {{ alertMessage }}
+          </b-alert>
+          <b-alert :show="isBusy" dismissible variant="warning">
+            {{ alertMessage }}
+          </b-alert>
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import statuses from '@/modules/statuses'
+
+export default {
+  data() {
+    return {
+      status: statuses.Idle,
+      alertMessage: '',
+    }
+  },
+  computed: {
+    // TODO: abstract and create functions dynamically.
+    isIdle() {
+      return this.status === statuses.Idle
+    },
+    isSuccess() {
+      return this.status === statuses.Success
+    },
+    isError() {
+      return this.status === statuses.Error
+    },
+    isBusy() {
+      return this.status === statuses.Busy
+    }
+  },
+  methods: {
+    setStatus(status, message) {
+      this.status = statuses[status]
+      this.alertMessage = message
+    },
+    printLabels() {
+      return
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ test/pages/print_destination_plate_labels.spec.js
@@ -0,0 +1,24 @@
+import BootstrapVue from 'bootstrap-vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+import PrintDestinationPlateLabels from '@/pages/print_destination_plate_labels'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+describe('print destination plate labels', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(PrintDestinationPlateLabels, {
+      localVue,
+      data() {
+        return {}
+      }
+    })
+  })
+
+  it('is a Vue instance', () => {
+    expect(wrapper.findComponent(PrintDestinationPlateLabels).exists()).toBeTruthy()
+  })
+
+})
