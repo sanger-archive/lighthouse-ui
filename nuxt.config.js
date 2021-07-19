daae5c2900161ab485aafa764cc3fde56caabf86
Harriet Craven <hc6@sanger.ac.uk>
2021-07-19 17:08:49 +0100

    uncomment

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    // '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module',
     '@nuxtjs/dotenv',
   ],
   /*
