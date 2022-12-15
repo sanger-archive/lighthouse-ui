82ef82e00730f6248f557eaf949ba1d544b737cd
Stuart McHattie <sm49@sanger.ac.uk>
2022-12-15 17:03:13 +0000

    Switch to bootstrap-vue-3

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,10 +59,7 @@ export default {
    ** Nuxt.js modules
    */
   modules: [
-    // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    'bootstrap-vue-3/nuxt',
   ],
   /*
    ** Axios module configuration
